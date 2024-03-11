c3bec70eb3c04e09760e164c49d18cd52c433c01
Kyu0 <efftar@naver.com>
2024-03-11 19:22:00 +0900

    styling popup.html styling sign in button added loading indicator

--- src/popup.ts
+++ src/popup.ts
@@ -1,9 +1,21 @@
 import $ from 'jquery';
 
+const $loading = $('.loading');
+
+function showTag($tag: JQuery<HTMLElement>) {
+    $tag.removeClass('hide');
+}
+
+function hideTag($tag: JQuery<HTMLElement>) {
+    $tag.addClass('hide');
+}
+
 $('.oauth.github').on('click', async () => {
+    showTag($loading);
     const data = await new Promise((resolve) => {
         chrome.runtime.sendMessage({ msg: 'LOGIN_GITHUB' }, response => {
             resolve(response);
         })
     });
+    hideTag($loading);
 });
